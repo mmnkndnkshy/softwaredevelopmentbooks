8f5a703da9ecbbc792f4e9e3a1561d4d75946da7
Manikandan Muthuchamy <manikandanmuthuchamy@gmail.com>
2026-04-21 22:34:31 +0530

    feat: Add Rest Endpoint for getting software development books

--- src/main/java/com/mmnkndn/kata/softwaredevelopmentbooks/controller/SoftwareDevelopmentBooksController.java
+++ src/main/java/com/mmnkndn/kata/softwaredevelopmentbooks/controller/SoftwareDevelopmentBooksController.java
@@ -1,7 +1,15 @@
 package com.mmnkndn.kata.softwaredevelopmentbooks.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/softwaredevelopmentbooks")
 public class SoftwareDevelopmentBooksController {
+
+    @GetMapping("/getbooks")
+    public String getBooks(){
+        return "Success";
+    }
 }
--- src/test/java/com/mmnkndn/kata/softwaredevelopmentbooks/controller/SoftwareDevelopmentBooksControllerTest.java
+++ src/test/java/com/mmnkndn/kata/softwaredevelopmentbooks/controller/SoftwareDevelopmentBooksControllerTest.java
@@ -4,8 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
 public class SoftwareDevelopmentBooksControllerTest {
@@ -13,9 +19,18 @@ public class SoftwareDevelopmentBooksControllerTest {
     @Autowired
     private SoftwareDevelopmentBooksController softwareDevelopmentBooksController;
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     @DisplayName("SoftwareDevelopmentBooks controller bean should not be null")
     void softwareDevelopmentBooksController_shouldNotBeNull(){
         assertThat(softwareDevelopmentBooksController).isNotNull();
+    }
+
+    @Test
+    @DisplayName("API getBooks should return status ok")
+    void getBooks_Api_shouldReturn_statusOk() throws Exception {
+        mockMvc.perform(get("/api/softwaredevelopmentbooks/getbooks")).andExpect(status().isOk());
     }
 }
