68330ca6681706126893d59bcc8bf3c9f367163c
Manikandan Muthuchamy <manikandanmuthuchamy@gmail.com>
2026-04-22 13:38:37 +0530

    feat: calculate price should return price based on quantities

--- src/main/java/com/mmnkndn/kata/softwaredevelopmentbooks/service/BookPricingService.java
+++ src/main/java/com/mmnkndn/kata/softwaredevelopmentbooks/service/BookPricingService.java
@@ -1,6 +1,7 @@
 package com.mmnkndn.kata.softwaredevelopmentbooks.service;
 
 import com.mmnkndn.kata.softwaredevelopmentbooks.catalog.SoftwareDevelopmentBook;
+import com.mmnkndn.kata.softwaredevelopmentbooks.dto.BookDto;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -11,8 +12,8 @@ import java.util.stream.Collectors;
 @Service
 public class BookPricingService {
 
-    public Double bookPricing(int bookId) {
+    public Double bookPricing(BookDto bookDto) {
         Map<Integer, Double> bookIdPriceMap = Arrays.stream(SoftwareDevelopmentBook.values()).collect(Collectors.toMap(SoftwareDevelopmentBook::getId, SoftwareDevelopmentBook::getPrice));
-        return bookIdPriceMap.get(bookId);
+        return (bookIdPriceMap.get(bookDto.getId()) * bookDto.getNoOfBooks());
     }
 }
--- src/test/java/com/mmnkndn/kata/softwaredevelopmentbooks/service/BookPricingServiceTest.java
+++ src/test/java/com/mmnkndn/kata/softwaredevelopmentbooks/service/BookPricingServiceTest.java
@@ -1,7 +1,10 @@
 package com.mmnkndn.kata.softwaredevelopmentbooks.service;
 
+import com.mmnkndn.kata.softwaredevelopmentbooks.dto.BookDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class BookPricingServiceTest {
-    private static final int BOOK_ID = 1;
+    private static final int ONE = 1;
     private static final double BOOK_PRICE = 50.00;
 
     @Autowired
@@ -18,7 +21,23 @@ public class BookPricingServiceTest {
     @Test
     @DisplayName("book pricing for a book should return 50")
     void bookPricingForABook_shouldReturnFifty() {
-        Double actualPrice = bookPricingService.bookPricing(BOOK_ID);
+
+        BookDto bookDto = new BookDto(ONE, ONE);
+
+        Double actualPrice = bookPricingService.bookPricing(bookDto);
+
         assertEquals(BOOK_PRICE, actualPrice);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 50.00", "2, 100.00", "3, 150.00", "4, 200.00", "5, 250.00"})
+    @DisplayName("book pricing should return price based on quantity")
+    void bookPricing_shouldReturnPriceBasedOnQuantity(int noOfBooks, double expectedPrice) {
+
+        BookDto bookDto = new BookDto(ONE, noOfBooks);
+
+        Double actualPrice = bookPricingService.bookPricing(bookDto);
+
+        assertEquals(expectedPrice, actualPrice);
     }
 }
