7fc4f34e2dacdeda9fa26742cc63189fd322f5f6
Manikandan Muthuchamy <manikandanmuthuchamy@gmail.com>
2026-04-22 08:51:36 +0530

    upgrade Spring Boot to 3.5.13 and replace deprecated @MockBean in controller tests

--- src/test/java/com/mmnkndn/kata/softwaredevelopmentbooks/controller/SoftwareDevelopmentBooksControllerTest.java
+++ src/test/java/com/mmnkndn/kata/softwaredevelopmentbooks/controller/SoftwareDevelopmentBooksControllerTest.java
@@ -1,19 +1,20 @@
 package com.mmnkndn.kata.softwaredevelopmentbooks.controller;
 
 import com.mmnkndn.kata.softwaredevelopmentbooks.service.SoftwareDevelopmentBooksService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-
-
-
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = SoftwareDevelopmentBooksController.class)
@@ -23,17 +24,22 @@ public class SoftwareDevelopmentBooksControllerTest {
     private String GETBOOKS_ENPOINT;
 
     @Autowired
-    private SoftwareDevelopmentBooksController softwareDevelopmentBooksController;
-
-    @MockBean
-    private SoftwareDevelopmentBooksService softwareDevelopmentBooksService;
+    private MockMvc mockMvc;
 
     @Autowired
-    private MockMvc mockMvc;
+    private SoftwareDevelopmentBooksController softwareDevelopmentBooksController;
+
+    @Autowired
+    private SoftwareDevelopmentBooksService softwareDevelopmentBooksService;
+
+    @BeforeEach
+    void setup() {
+        Mockito.reset(softwareDevelopmentBooksService);
+    }
 
     @Test
     @DisplayName("SoftwareDevelopmentBooks controller bean should not be null")
-    void softwareDevelopmentBooksController_shouldNotBeNull(){
+    void softwareDevelopmentBooksController_shouldNotBeNull() {
         assertThat(softwareDevelopmentBooksController).isNotNull();
     }
 
@@ -41,5 +47,13 @@ public class SoftwareDevelopmentBooksControllerTest {
     @DisplayName("API getBooks should return status ok")
     void getBooks_Api_shouldReturn_statusOk() throws Exception {
         mockMvc.perform(get(GETBOOKS_ENPOINT)).andExpect(status().isOk());
+    }
+
+    @TestConfiguration
+    static class SoftwareDevelopmentBooksControllerTestContextConfiguration {
+        @Bean
+        public SoftwareDevelopmentBooksService softwareDevelopmentBooksService() {
+            return Mockito.mock(SoftwareDevelopmentBooksService.class);
+        }
     }
 }
